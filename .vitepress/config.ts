64ba355fbf184ffc0dff687440393df1fa3e60de
Trofimov Maxim <me@dantezz.ru>
2024-11-15 18:02:53 +0700

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Lite-SSR Docs",
   description: "Documentation for Lite-SSR",
+  base: "/lite-ssr-docs/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
